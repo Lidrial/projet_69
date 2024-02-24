b05d4986f65b8c3fe84d8c773c242b44d50957b2
Arsanator <asdead83@gmail.com>
2024-02-24 23:00:38 +0100

    Mouvement Prototype 2
    idle, run, jump et flip du personnage fonctionnel (problème précédent réglé)

--- PrototypeBody2D.gd
+++ PrototypeBody2D.gd
@@ -16,54 +16,46 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	# Handle jump.
 	var jumpInput = Input.is_action_just_pressed("ui_accept");
-	if jumpInput and is_on_floor():
-		jump()
+	
+	# Handle Direction.
+	direction = Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+			_animated_sprite.flip_h = false
+	elif direction < 0:
+			_animated_sprite.flip_h = true
 
 	# Handle Run.
-	direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0 && _animated_sprite.animation != "4-Jump":
+	if direction and !jumpInput: 
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	
-	if direction:
-		velocity.x = direction * SPEED
-		if direction > 0 and is_on_floor():
-			_animated_sprite.flip_h = false
+		if direction != 0 and is_on_floor():
 			_animated_sprite.play("2-Run")
-		elif direction < 0 and is_on_floor():
-			_animated_sprite.flip_h = true
-			_animated_sprite.play("2-Run")
-		elif direction > 0:
-			_animated_sprite.flip_h = false
-		elif direction < 0:
-			_animated_sprite.flip_h = true
-		
-		
-		
-		
+
 	# Handle Idle.
 	elif !direction and !jumpInput and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		_animated_sprite.play('1-Idle')
+	
+	# Handle Jump.
+	elif jumpInput and is_on_floor() :
+		jump()
 
 	move_and_slide()
-	#update_animation()
-	
-func update_animation():
-	if not animation_locked:
-		if not is_on_floor():
-			_animated_sprite.play("4-Jump")
-		else:
-			if direction:
-				_animated_sprite.play("2-Run")
-			else:
-				_animated_sprite.play("1-Idle")
+
+	#
+#func update_animation():
+	#if not animation_locked:
+		#if not is_on_floor():
+			#_animated_sprite.play("4-Jump")
+		#else:
+			#if direction:
+				#_animated_sprite.play("2-Run")
+			#else:
+				#_animated_sprite.play("1-Idle")
 	
 func jump():
 		velocity.y = JUMP_VELOCITY
+		_animated_sprite
 		_animated_sprite.play("4-Jump")
 		animation_locked = true
+		
