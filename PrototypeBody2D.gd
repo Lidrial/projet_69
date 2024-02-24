b47466a86b4ab4f30dcb37647e7f02535ba11244
Arsanator <asdead83@gmail.com>
2024-02-25 00:26:17 +0100

    Mouvement prototype 3
    Ajout mouvement Walk

--- PrototypeBody2D.gd
+++ PrototypeBody2D.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED_WALK = 75.0
+const SPEED_RUN = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -16,42 +17,42 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	var jumpInput = Input.is_action_just_pressed("ui_accept");
+	var jump_input = Input.is_action_just_pressed("input_space");
+
 	
 	# Handle Direction.
-	direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("input_move_left", "input_move_right")
+	var shift_input = Input.is_action_pressed("input_shift")
+	
 	if direction > 0:
 			_animated_sprite.flip_h = false
 	elif direction < 0:
 			_animated_sprite.flip_h = true
 
+	# Handle Walk.
+	if direction and not jump_input and not shift_input: 
+		velocity.x = direction * SPEED_WALK
+		if direction != 0 and is_on_floor():
+			_animated_sprite.play("3-Walk")
+			
 	# Handle Run.
-	if direction and !jumpInput: 
-		velocity.x = direction * SPEED
+	elif direction and not jump_input and shift_input: 
+		velocity.x = direction * SPEED_RUN
 		if direction != 0 and is_on_floor():
 			_animated_sprite.play("2-Run")
 
 	# Handle Idle.
-	elif !direction and !jumpInput and is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif not direction and not jump_input and is_on_floor():
+
+		velocity.x = move_toward(velocity.x, 0, SPEED_RUN)
 		_animated_sprite.play('1-Idle')
 	
 	# Handle Jump.
-	elif jumpInput and is_on_floor() :
+	elif jump_input and is_on_floor() :
 		jump()
 
 	move_and_slide()
 
-	#
-#func update_animation():
-	#if not animation_locked:
-		#if not is_on_floor():
-			#_animated_sprite.play("4-Jump")
-		#else:
-			#if direction:
-				#_animated_sprite.play("2-Run")
-			#else:
-				#_animated_sprite.play("1-Idle")
 	
 func jump():
 		velocity.y = JUMP_VELOCITY
