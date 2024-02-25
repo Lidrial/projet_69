3a69cc4345ef9686577debf8ab5edac8b6bad450
royjah <eltonroy.h@gmail.com>
2024-02-25 14:27:54 +0100

    résolution bug multiple saut cthulu

--- CthuluBody2D.gd
+++ CthuluBody2D.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 const SPEED = 150.0
 var mouvementX = 0
 var saut = 0
-var OneOrTwo
+var action
 var temp = 10
 const JUMP_VELOCITY = -400.0
+var HasJumped = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,28 +22,35 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	#pick a random number between 1 and 2 every 10 frames
 	if temp == 0:
-		OneOrTwo = randi_range(1,4)
-		print(OneOrTwo)
-		#OneOrTwo = 4
+		action = randi_range(1,4)
+		HasJumped = false		
+		print(action)
 		temp = 60
 	else:
 		temp -= 1
-	#OneOrTwo = randi_range(1,2)
-	if OneOrTwo == 1:
+	#action = randi_range(1,2)
+	if action == 1:
 		%CthuluBody2D.position.x += 1
 		%AnimatedCthulu2D.flip_h = false
 		%AnimatedCthulu2D.play("walk")
+		#HasJumped = false
 		
-	elif OneOrTwo == 2:
+		
+	elif action == 2:
 		%CthuluBody2D.position.x += -1
 		%AnimatedCthulu2D.flip_h = true
 		%AnimatedCthulu2D.play("walk")
-	elif OneOrTwo == 3:
+		#HasJumped = false
+		
+	elif action == 3:
 		%CthuluBody2D.position.x += 0
 		%AnimatedCthulu2D.play("idle")
-	elif is_on_floor(): 
+		#HasJumped = false
+		
+	elif is_on_floor() and HasJumped == false: 
 		%AnimatedCthulu2D.play("fly")
 		velocity.y = JUMP_VELOCITY
+		HasJumped = true 
 	pass
 
 	move_and_slide()
